423c6f84bb5a79dc11f3ecd1813356bb13b315a9
KevinKaras <kevinkaras2@gmail.com>
2021-02-01 17:17:39 -0600

    finished preorder arry

--- problems/02-tree-order.js
+++ problems/02-tree-order.js
@@ -2,7 +2,17 @@
 // and returns an array containing the values from a pre-order
 // traversal.
 function preOrderArray(root) {
-    // Your code here
+    let newArr = [];
+    if (root === null){
+        return [];
+    }
+    
+    newArr.push(root.val)
+    let miniArr1 = preOrderArray(root.left);   // []
+    let miniArr2 = preOrderArray(root.right);  // []
+    let combinedArr = miniArr1.concat(miniArr2)
+    newArr = newArr.concat(combinedArr);
+    return newArr;
 }
 
 // Implement a function that takes the root of a Binary Tree
